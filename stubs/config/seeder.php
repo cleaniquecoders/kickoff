e7c6fe4201c60d1a1eee32be815419eb7a42731d
Nasrul Hazim <nasrulhazim.m@gmail.com>
2025-08-03 15:51:39 +0800

    Update seeder.php

--- stubs/config/seeder.php
+++ stubs/config/seeder.php
@@ -6,7 +6,6 @@ return [
             'name' => env('SUPERADMIN_NAME', 'Superadmin'),
             'email' => env('SUPERADMIN_EMAIL'),
             'password' => env('SUPERADMIN_PASSWORD'),
-            'password_confirmation' => env('SUPERADMIN_PASSWORD'),
         ],
     ],
 ];
